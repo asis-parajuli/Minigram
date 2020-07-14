0c695221add7ec3591078e3ac97aba044d2b4cf0
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-07-14 16:27:28 +0545

    Starter Basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './minigram.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src={logo} alt="logo"></img>
+      </div>
+      <h1>Starter</h1>
+
     </div>
   );
 }
